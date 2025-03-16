fd6f0d59f25fd196fe136177aefd9fb62565a2bb
Augustus D'Souza <augustus@clergo.com>
2025-03-17 05:25:58 +0530

    Update vitepress config

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -13,6 +13,8 @@ export default withMermaid({
     nav: [
       { text: "Home", link: "/" },
       { text: "Guide", link: "/guide/intro" },
+      { text: "Reference", link: "/reference/core" },
+      { text: "Examples", link: "/examples/webhook-services" },
     ],
 
     sidebar: {
@@ -25,6 +27,83 @@ export default withMermaid({
             { text: "Basic Overview", link: "/guide/basic-overview" },
             { text: "Webhooks", link: "/guide/webhook-pattern" },
             { text: "Why LeanMQ?", link: "/guide/why-leanmq" },
+          ],
+        },
+        {
+          text: "Advanced",
+          items: [
+            {
+              text: "Queue Management",
+              link: "/guide/advanced/queue-management",
+            },
+            {
+              text: "Atomic Transactions",
+              link: "/guide/advanced/transactions",
+            },
+            {
+              text: "Error Handling",
+              link: "/guide/advanced/error-handling",
+            },
+            {
+              text: "Scaling",
+              link: "/guide/advanced/scaling",
+            },
+            {
+              text: "Production",
+              link: "/guide/advanced/production",
+            },
+          ],
+        },
+      ],
+
+      "/reference/": [
+        {
+          text: "Reference",
+          items: [
+            { text: "Core", link: "/reference/core" },
+            { text: "Webhook", link: "/reference/webhook" },
+            { text: "Queue", link: "/reference/queue" },
+            { text: "Message", link: "/reference/message" },
+            { text: "Transaction", link: "/reference/transaction" },
+            { text: "Exceptions", link: "/reference/exceptions" },
+          ],
+        },
+      ],
+      "/examples/": [
+        {
+          text: "Examples",
+          items: [
+            { text: "Webhook Services", link: "/examples/webhook-services" },
+            { text: "Basic Messaging", link: "/examples/basic-messaging" },
+          ],
+        },
+        {
+          text: "Full Microservice",
+          items: [
+            {
+              text: "Defining the Event System",
+              link: "/examples/microservice/defining-the-event-system",
+            },
+            {
+              text: "Event Bus Implementation",
+              link: "/examples/microservice/event-bus-implementation",
+            },
+            {
+              text: "Microservice Implementation",
+              link: "/examples/microservice/microservice-implementation",
+            },
+            {
+              text: "Implementing Specific Microservices",
+              link: "/examples/microservice/implementing-specific-microservices",
+            },
+            {
+              text: "Implementing Shipping and Notification Services",
+              link: "/examples/microservice/implementing-shipping-and-notification-services",
+            },
+            {
+              text: "Running the Event-Driven System",
+              link: "/examples/microservice/running-the-event-driven-system",
+            },
           ],
         },
       ],
